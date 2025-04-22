c8e60b4a51bf6f79f6727f82fdcb08c0fc4b4926
Mihnea-Octavian Manolache <mihnea.manolache@yahoo.com>
2025-04-22 12:43:23 +0300

    Fix faulty request

--- driver.js
+++ driver.js
@@ -345,7 +345,6 @@ class Driver {
             userAgent:
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
             extended: false,
-            debug: options.debug ?? process.env.DEBUG !== undefined,
             ...options,
         }
 
